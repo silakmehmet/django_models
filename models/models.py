99de3807a244ac307b85dacb1d72e1045e54f75e
silakmehmet <silakmehmet@gmail.com>
2024-07-09 13:35:11 +0300

    First model created and registered

--- models/models.py
+++ models/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=25)
+    number = models.PositiveSmallIntegerField(unique=True)
+    about = models.TextField(blank=True)
+    email = models.EmailField()
+    register_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.number}"
